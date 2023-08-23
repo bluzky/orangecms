3376182a10a248ccd1435a0e6bdfa25d34890d3d
abhishek <abhishek.tripathi456@gmail.com>
2023-08-23 13:34:01 +0530

    minor bug in mix.exs

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule OrangeCms.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "db.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
